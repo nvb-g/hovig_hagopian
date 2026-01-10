eefbae0ff1591ac140f0b6176566cff900adf240
Selectarome Marketing <marketing@selectarome.com>
2026-01-10 15:43:17 +0100

    cdn tru

--- src/lib/sanity/client.ts
+++ src/lib/sanity/client.ts
@@ -7,5 +7,5 @@ export const client = createClient({
   projectId,
   dataset,
   apiVersion, // https://www.sanity.io/docs/api-versioning
-  useCdn: false, // Set to false if statically generating pages, using ISR or tag-based revalidation
+  useCdn: true, // Set to false if statically generating pages, using ISR or tag-based revalidation
 });
